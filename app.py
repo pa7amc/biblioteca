279a903a0cc3829a3821bd1abd31d5f7147e2eaa
Patrícia Correia <pa7amc@gmail.com>
2021-01-11 19:59:12 +0000

    forms e html skeleton pt1

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import os
+import sqlalchemy
 
 app = Flask(__name__)
 
@@ -7,6 +8,37 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def home():
     return render_template('home.html')
+
+
+@app.route('/campanhas', methods=['GET'])
+def campanhas():
+    return render_template('campanhas.html')
+
+@app.route('/compl_req', methods=['GET'])
+def compl_req():
+    return render_template('compl_req.html')
+
+@app.route('/reg_livro', methods=['GET'])
+def reg_livro():
+    return render_template('reg_livro.html')
+
+
+@app.route('/reg_req', methods=['GET'])
+def reg_req():
+    return render_template('reg_req.html')
+
+
+@app.route('/reg_soc', methods=['GET'])
+def reg_soc():
+    return render_template('reg_soc.html')
+
+@app.route('/ver_req', methods=['GET'])
+def ver_req():
+    return render_template('ver_req.html')
+
+@app.route('/ver_soc', methods=['GET'])
+def ver_soc():
+    return render_template('ver_soc.html')
 
 
 
